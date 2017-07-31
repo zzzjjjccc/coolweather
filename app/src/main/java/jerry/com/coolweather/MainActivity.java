fa68c858e3f31720117595280a87d2973a34c3d6
Jerry <2218982471@qq.com>
2017-07-31 16:33:07 +0800

    First commit.

---
+++ app/src/main/java/jerry/com/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package jerry.com.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
